932a11baaadf671c05f0a66401892ac0ecf5152e
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-05-06 23:22:45 -0300

    Inicio da criação do boneco

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,67 @@ function Scene() {
   const mesh = useRef()
 
   useFrame(() => {
-    mesh.current.rotation.x += 0.01
     mesh.current.rotation.y += 0.01
   })
 
   return (
     <>
-      <mesh ref={mesh}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="#00f0f0" />
-      </mesh>
-      <mesh position={[2, 0, 0]}>
-        <sphereGeometry args={[0.5, 32, 32]} />
-        <meshStandardMaterial color="#f00" />
-      </mesh>
+      {/* Corpo do boneco */}
+      <group ref={mesh}>
+        {/* Cabeça */}
+        <mesh position={[0, 1.5, 0]}>
+          <sphereGeometry args={[0.5, 32, 32]} />
+          <meshStandardMaterial color="#fae7d7" />
+        </mesh>
+
+        {/* Olho esquerdo */}
+        <mesh position={[-0.15, 1.4, 0.45]}>
+          <sphereGeometry args={[0.1, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/* Olho direito */}
+        <mesh position={[0.15, 1.4, 0.45]}>
+          <sphereGeometry args={[0.1, 16, 16]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+
+        {/* Corpo */}
+        <mesh position={[0, 0.5, 0]}>
+          <cylinderGeometry args={[0.4, 0.35, 1, 32]} />
+          <meshStandardMaterial color="#030496" />
+        </mesh>
+
+        {/* Braço esquerdo */}
+        <mesh position={[-0.555, 0.5, 0]} rotation={[0, 0, Math.PI / 1.1]}>
+          <cylinderGeometry args={[0.1, 0.1, 0.8, 32]} />
+          <meshStandardMaterial color="#fae7d7" />
+        </mesh>
+
+        {/* Braço direito */}
+        <mesh position={[0.6, 1.2, 0.3]} rotation={[10, 10, Math.PI / 1]}>
+          <cylinderGeometry args={[0.1, 0.1, 0.8, 32]} />
+          <meshStandardMaterial color="#fae7d7" />
+        </mesh>
+
+        {/* Perna esquerda */}
+        <mesh position={[-0.2, -0.5, 0]}>
+          <cylinderGeometry args={[0.15, 0.12, 1, 32]} />
+          <meshStandardMaterial color="#020202" />
+        </mesh>
+
+        {/* Perna direita */}
+        <mesh position={[0.2, -0.5, 0]}>
+          <cylinderGeometry args={[0.15, 0.12, 1, 32]} />
+          <meshStandardMaterial color="#020202" />
+        </mesh>
+
+        {/* Cabelos */}
+        <mesh position={[0, 1.6, 0]}>
+          <sphereGeometry args={[0.55, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+      </group>
     </>
   )
 }
@@ -42,7 +89,7 @@ function CanvasContainer() {
   return (
     <div className="canvas-container">
       <Canvas>
-        <ambientLight />
+        <ambientLight intensity={1.5} />
         <pointLight position={[10, 10, 10]} />
         <OrbitControls />
         <Scene />
